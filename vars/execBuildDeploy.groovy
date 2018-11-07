a47db5040cdcf811aa865b61aa569a1062260cc7
Lucas Stramandinoli <lucasstramandinoli@MBP-de-Lucas.fritz.box>
2018-11-07 17:10:30 +0100

    testing declarativ script 2

--- vars/execBuildDeploy.groovy
+++ vars/execBuildDeploy.groovy
@@ -1,28 +1,38 @@
 #!groovy
 def call(String command){
   if (command == "build"){
-    stages {
-      stage('Build') {
-        steps {
-          echo "Building app"
+    pipeline {
+      agent any
+        stages {
+          stage('Build') {
+            steps {
+              echo "Building app"
+            }
+          }
         }
       }
     }
   }
   else if (command == "deploy"){
-    stages {
-      stage('Deploy') {
-        steps {
-          echo "Deploying app"
+    pipeline {
+      agent any
+      stages {
+        stage('Deploy') {
+          steps {
+            echo "Deploying app"
+          }
         }
       }
     }
   }
   else{
-    stages {
-      stage('CommandFail') {
-        steps {
-          echo "Command not found.."
+    pipeline {
+      agent any
+      stages {
+        stage('CommandFail') {
+          steps {
+            echo "Command not found.."
+          }
         }
       }
     }
